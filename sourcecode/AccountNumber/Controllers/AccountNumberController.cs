65d2adca09da196d1c0284812b6065c674778f29
samia-iqbal <samia.iqbal@avanade.com>
2021-06-09 23:59:05 +0100

    names and account numbers service are ready

---
+++ sourcecode/AccountNumber/Controllers/AccountNumberController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AccountNumber.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountNumberController : ControllerBase
+    {
+        private static readonly int[] Numbers = new[]
+        {
+            1,2,3,4,5,6,7,8,9,0
+        };
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var rnd = new Random();
+            var returnIndex = rnd.Next(0, 9);
+            return Numbers[returnIndex].ToString();
+        }
+    }
+}
---
+++ sourcecode/Merge/Controllers/MergeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Merge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        //accountNumbers : https://localhost:44315/
+        //nameURL: https://localhost:44311/
+    }
+}

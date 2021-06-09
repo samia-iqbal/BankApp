40be6e512550bb5ea47865b06a34636e1136507e
samia-iqbal <samia.iqbal@avanade.com>
2021-06-10 00:05:59 +0100

    service three is done

--- sourcecode/Merge/Controllers/MergeController.cs
+++ sourcecode/Merge/Controllers/MergeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Merge.Controllers
@@ -13,5 +15,21 @@ namespace Merge.Controllers
     {
         //accountNumbers : https://localhost:44315/
         //nameURL: https://localhost:44311/
+
+        private IConfiguration Configuration;
+        public MergeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var numbersService = "https://localhost:44315/";
+            var serviceOneResponseCall = await new HttpClient().GetStringAsync(numbersService);
+            var lettersService = "https://localhost:44311/";
+            var serviceTwoResponseCall = await new HttpClient().GetStringAsync(lettersService);
+            var mergedResponse =  $"{serviceOneResponseCall}{serviceTwoResponseCall}";
+            return Ok(mergedResponse);
+        }
     }
 }
